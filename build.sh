c3b3697d17406316862840d641db22ff0b36dcc7
Thar Htet <kotharthar@gmail.com>
2016-03-10 23:00:07 +0630

    Project version 1.

---
+++ build.sh
@@ -0,0 +1,5 @@
+rm -rf jsoneditor.app
+cp -R nwjs.app jsoneditor.app
+cp -R bower_components jsoneditor.app/Contents/Resources/bower_components
+cp index.html jsoneditor.app/Contents/Resources/index.html
+cp package.json jsoneditor.app/Contents/Resources/package.json
