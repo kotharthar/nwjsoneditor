817eb3be4a8fb56e4160e9f77789412d329965be
Thar Htet <kotharthar@gmail.com>
2016-03-11 15:33:13 +0630

    shortcuts added. Icons placed in packagin.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 rm -rf jsoneditor.app
 cp -R nwjs.app jsoneditor.app
-cp -R bower_components jsoneditor.app/Contents/Resources/bower_components
-cp index.html jsoneditor.app/Contents/Resources/index.html
-cp package.json jsoneditor.app/Contents/Resources/package.json
+mkdir -p jsoneditor.app/Contents/Resources/app.nw
+cp -R bower_components jsoneditor.app/Contents/Resources/app.nw/bower_components
+cp index.html jsoneditor.app/Contents/Resources/app.nw/index.html
+cp package.json jsoneditor.app/Contents/Resources/app.nw/package.json
+cp json.icns jsoneditor.app/Contents/Resources/nw.icns
